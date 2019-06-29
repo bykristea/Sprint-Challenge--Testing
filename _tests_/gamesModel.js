44604258eace6f0620ee71aca4ea261c3d6d2db1
Kristea Gamel <krgamel@gmail.com>
2019-06-29 12:25:04 -0500

    basic layout for tests

--- _tests_/gamesModel.js
+++ _tests_/gamesModel.js
@@ -1,1 +1,10 @@
-const games = require('../games/gamesModel');
+const games = require("./gamesModel");
+const db = require("../data/dbConfig.js");
+
+afterEach(async () => {
+    await db("games").truncate();
+  });
+
+describe("the games model", () => {
+
+})
