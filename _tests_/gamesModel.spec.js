32dd83d1218362838324fcbf12b5ef2280e6ea62
Kristea Gamel <krgamel@gmail.com>
2019-06-29 12:58:13 -0500

    renamed to get test to run

---
+++ _tests_/gamesModel.spec.js
@@ -0,0 +1,21 @@
+const games = require("../games/gamesModel.js");
+const db = require("../data/dbConfig.js");
+
+beforeEach(async () => {
+    await db("games").truncate();
+  });
+
+describe("the games model", () => {
+
+    it('should retrieve the rows/array of games', async () => {
+        const rows = await games.fetch();
+        expect(rows).toEqual([]);
+    });
+
+    it('should insert/add a game to database', async () => {
+        const ids = await games.insert({title: 'The Elder Scrolls V: Skyrim', genre: 'Action RPG', releaseYear: 2011});
+
+        expect(ids.length).toBe(1);
+        expect(ids[0]).toBe(1);
+    });
+});
---
+++ _tests_/server.spec.js
@@ -0,0 +1,30 @@
+const db = require('../data/dbConfig');
+const request = require("supertest");
+const server = require("../api/server.js");
+
+
+beforeEach(async () => {
+    await db("games").truncate();
+  });
+
+describe("the route handlers", () => {
+  describe("get /games", () => {
+    it("responds with 200", async () => {
+        const response = await request(server).get("/games");
+        expect(response.status).toBe(200);
+      });
+
+      it("responds with json", async () => {
+        const response = await request(server).get("/games");
+        expect(response.type).toMatch(/json/i);
+      });
+      
+      it("sends the correct response object", async () => {
+      const response = await request(server).get("/games");
+      expect(response.body).toEqual([]);
+    });
+  })
+  describe("post /games", () => {
+
+  })
+})
